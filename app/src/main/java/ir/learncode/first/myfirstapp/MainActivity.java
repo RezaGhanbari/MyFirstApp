8f1c5026eb370067ba115f9659cba031a004962c
Reza <rezapython@gmail.com>
2016-08-22 01:44:48 +0430

    initial commit

---
+++ app/src/main/java/ir/learncode/first/myfirstapp/MainActivity.java
@@ -0,0 +1,26 @@
+package ir.learncode.first.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "ir.learncode.first.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent     = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message    = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
